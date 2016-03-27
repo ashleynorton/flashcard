e22be78d40a4038d0041079b1b823a239591858e
Ashley Norton <ashleytheengineer@gmail.com>
2016-03-27 15:37:35 -0400

    Add jQuery functions to each card to make it toggle

---
+++ js/scripts.js
@@ -0,0 +1,66 @@
+$(function() {
+  $("#card1").click(function() {
+    $("#card1 .initially-hidden").toggle();
+    $("#card1 .initially-showing").toggle();
+  });
+  $("#card2").click(function() {
+    $("#card2 .initially-hidden").toggle();
+    $("#card2 .initially-showing").toggle();
+  });
+   $("#card3").click(function() {
+    $("#card3 .initially-hidden").toggle();
+    $("#card3 .initially-showing").toggle();
+  });
+  $("#card4").click(function() {
+    $("#card4 .initially-hidden").toggle();
+    $("#card4 .initially-showing").toggle();
+  });
+  $("#card5").click(function() {
+    $("#card5 .initially-hidden").toggle();
+    $("#card5 .initially-showing").toggle();
+  });
+  $("#card6").click(function() {
+    $("#card6 .initially-hidden").toggle();
+    $("#card6 .initially-showing").toggle();
+  });
+  $("#card7").click(function() {
+    $("#card7 .initially-hidden").toggle();
+    $("#card7 .initially-showing").toggle();
+  });
+  $("#card8").click(function() {
+    $("#card8 .initially-hidden").toggle();
+    $("#card8 .initially-showing").toggle();
+  });
+   $("#card9").click(function() {
+    $("#card9 .initially-hidden").toggle();
+    $("#card9 .initially-showing").toggle();
+  });
+  $("#card10").click(function() {
+    $("#card10 .initially-hidden").toggle();
+    $("#card10 .initially-showing").toggle();
+  });
+  $("#card11").click(function() {
+    $("#card11 .initially-hidden").toggle();
+    $("#card11 .initially-showing").toggle();
+  });
+  $("#card12").click(function() {
+    $("#card12 .initially-hidden").toggle();
+    $("#card12 .initially-showing").toggle();
+  });
+  $("#card13").click(function() {
+    $("#card13 .initially-hidden").toggle();
+    $("#card13 .initially-showing").toggle();
+  });
+  $("#card14").click(function() {
+    $("#card14 .initially-hidden").toggle();
+    $("#card14 .initially-showing").toggle();
+  });
+  $("#card15").click(function() {
+    $("#card15 .initially-hidden").toggle();
+    $("#card15 .initially-showing").toggle();
+  });
+  $("#card16").click(function() {
+    $("#card16 .initially-hidden").toggle();
+    $("#card16 .initially-showing").toggle();
+  });
+});
